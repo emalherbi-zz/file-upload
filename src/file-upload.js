6b9b4d97013739b19094ea60070b068e79742a79
Eduardo Malherbi <emalherbi@gmail.com>
2018-01-12 14:42:05 -0200

    Fix some errors...

--- src/file-upload.js
+++ src/file-upload.js
@@ -1,30 +1,31 @@
 function fileUpload(inputName, types, _callback) {
+  'use strict';
   var input = document.getElementById(inputName);
   //
   if (!window.File || !window.FileReader || !window.FileList || !window.Blob) {
-    alert("Your browser doesn't support files.");
+    alert('Your browser doesn\'t support files.');
     return;
   }
   //
   if (!input.files) {
-    alert("Your browser doesn't support files.");
+    alert('Your browser doesn\'t support files.');
     return;
   }
   //
   if (input.files.length === 0) {
-    alert("Please, select a file to send!");
+    alert('Please, select a file to send!');
     return;
   }
   //
   for (var i = 0, il = input.files.length; i < il; i++) {
-    file = input.files[i];
+    var file = input.files[i];
     var xhr = new XMLHttpRequest();
     //
     if (xhr.upload) {
       //
       var type = file.name.split('.')[1];
       if (types.indexOf(type) === -1) {
-        alert("Only extensions are accepted: " + types + ".");
+        alert('Only extensions are accepted: ' + types + '.');
         return;
       }
       //
@@ -35,24 +36,24 @@ function fileUpload(inputName, types, _callback) {
             var fileName = xhr.responseText;
             _callback(fileName);
           } else {
-            console.log("Error", xhr.statusText);
+            console.log('Error', xhr.statusText);
           }
         }
       };
       //
-      xhr.addEventListener("load", function(e) {
+      xhr.addEventListener('load', function(e) {
         console.log('load');
       }, false);
       //
-      xhr.upload.addEventListener("progress", function(e) {
+      xhr.upload.addEventListener('progress', function(e) {
         console.log('progress');
       }, false);
       //
-      xhr.open("POST", 'file-upload.php', true);
-      xhr.setRequestHeader("X-FILENAME", file.name);
+      xhr.open('POST', 'file-upload.php', true);
+      xhr.setRequestHeader('X-FILENAME', file.name);
       xhr.send(file);
     }
-  };
+  }
   //
   return false;
 }
--- src/file-upload.php
+++ src/file-upload.php
@@ -1,26 +1,32 @@
 <?php
 
-defined("DS") || define("DS", DIRECTORY_SEPARATOR);
-//
+/*
+ * Eduardo Malherbi Martins (http://emalherbi.com/)
+ * Copyright @emm
+ * Full Stack Web Developer.
+ */
+
+defined('DS') || define('DS', DIRECTORY_SEPARATOR);
+
 $fileName = isset($_SERVER['HTTP_X_FILENAME']) ? $_SERVER['HTTP_X_FILENAME'] : false;
-//
+
 if (!empty($fileName)) {
-	$explode = explode('.', $fileName);
-	//
-	$rand1 = date('YmdHis');
-	$rand2 = rand(1, 100);
-	$rand3 = rand(1, 100);
-	//
-	$name = "File" . $rand1 . $rand2 . $rand3 . '.' . $explode[1];
-	$path = "file";
-	//
-	@mkdir($path, 0777, true);
-	@chmod($path, 0777);
-	//
-	file_put_contents($path . DS . $name, file_get_contents('php://input'));
-	//
-	echo $name;
-	exit;
+    $explode = explode('.', $fileName);
+
+    $rand1 = date('YmdHis');
+    $rand2 = rand(1, 100);
+    $rand3 = rand(1, 100);
+
+    $name = 'File'.$rand1.$rand2.$rand3.'.'.$explode[1];
+    $path = 'file';
+
+    @mkdir($path, 0777, true);
+    @chmod($path, 0777);
+
+    file_put_contents($path.DS.$name, file_get_contents('php://input'));
+
+    echo $name;
+    exit;
 }
 
 echo false;
